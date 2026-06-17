5208b9cb0b687df5d7005cb8870be9146fbe5da2
Vinicius de Souza <vinicius.souza110208@gmail.com>
2026-06-17 05:04:20 -0300

    Add files via upload

--- src/database.py
+++ src/database.py
@@ -6,9 +6,9 @@ load_dotenv()
 
 def conectar():
     return pymysql.connect(
-        host=os.getenv("DB_HOST"),
+        host=os.getenv("DB_HOST"),  
         port=int(os.getenv("DB_PORT")),
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD"),
-        database=os.getenv("DB_NAME")
+        database=os.getenv("DB_DATABASE")
     )
